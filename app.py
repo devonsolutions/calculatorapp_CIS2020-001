bbcfa95ce5f55bb578b378da4422007aa4112b80
devonsolutions <create@devon.solutions>
2025-12-17 16:28:31 -0800

    Created calculator pages
    Added arithmetic calculator

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask (__name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route("/matrix_addition")
+def matrix_addition():
+    return render_template('matrix_addition.html')
 
-@app.route("/matrix")
-def matrix():
-    return render_template('matrix.html')
+@app.route("/matrix_subtraction")
+def matrix_subtraction():
+    return render_template('matrix_subtraction.html')
 
 if __name__ == "__main__":
     app.run()
